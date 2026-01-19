e3328e24a0ef5ec760fc5095e65fa88ee25124fe
mac <mac@MacBook-Pro.local>
2026-01-19 19:49:28 +0200

    refactor: change background images from PNG to JPEG

--- src/features/avatar-background/mocks/backgrounds.ts
+++ src/features/avatar-background/mocks/backgrounds.ts
@@ -4,23 +4,23 @@ import type { BackgroundItemType } from "@/features/avatar-background/types";
 export const INITIAL_BACKGROUNDS: BackgroundItemType[] = [
   {
     id: "bg-1",
-    thumbnailUrl: `${BASE_URL}images/background-1.png`,
+    thumbnailUrl: `${BASE_URL}images/background-1.jpg`,
     isDefault: true,
   },
   {
     id: "bg-2",
-    thumbnailUrl: `${BASE_URL}images/background-2.png`,
+    thumbnailUrl: `${BASE_URL}images/background-2.jpg`,
   },
   {
     id: "bg-3",
-    thumbnailUrl: `${BASE_URL}images/background-3.png`,
+    thumbnailUrl: `${BASE_URL}images/background-3.jpg`,
   },
   {
     id: "bg-4",
-    thumbnailUrl: `${BASE_URL}images/background-4.png`,
+    thumbnailUrl: `${BASE_URL}images/background-4.jpg`,
   },
   {
     id: "bg-5",
-    thumbnailUrl: `${BASE_URL}images/background-5.png`,
+    thumbnailUrl: `${BASE_URL}images/background-5.jpg`,
   },
 ];
